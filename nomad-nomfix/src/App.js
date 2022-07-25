59437d9ff1800cb09d10e69eebf952012e6ee8ff
jomi707 <dol7966@gmail.com>
2022-07-25 15:38:54 +0900

    7.7, gh-pages, add script's code to deploy in package.json

--- nomad-nomfix/src/App.js
+++ nomad-nomfix/src/App.js
@@ -6,8 +6,16 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path="/"
+          element={<Home />}
+        ></Route>
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path="/movie/:id"
+          element={<Detail />}
+        ></Route>
       </Routes>
     </Router>
   );
