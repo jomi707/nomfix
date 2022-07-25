ce2b0d9ef24e57bc528fe65d26a0d5a06d7b6c42
jomi707 <dol7966@gmail.com>
2022-07-25 14:13:15 +0900

    7.5, use react-router-dom

--- nomad-nomfix/src/App.js
+++ nomad-nomfix/src/App.js
@@ -1,7 +1,25 @@
 import Home from "./home.js";
+import Detail from "./detail.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
+// url의 차이
+// 1. Hash Router: # url
+// 2. BrowswerRouter: nomal,
+
+// Routes: 안에 Route 두 개를 넣으면 동시에 렌더링 ???
+
+// a태그를 추가하여 route할 수도 있지만 페이지 전체가 재실행 됨
+// => Link를 이용
---
+++ nomad-nomfix/src/detail.js
@@ -0,0 +1,5 @@
+function Detail() {
+  return <h2>detail</h2>;
+}
+
+export default Detail;
