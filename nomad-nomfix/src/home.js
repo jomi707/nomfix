2254688bd13d5e1cdcdf959ac7e3d613d48c4ffe
jomi707 <dol7966@gmail.com>
2022-07-25 13:35:49 +0900

    7.4, 컴포넌트 분리

---
+++ nomad-nomfix/src/home.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// Hash Router
+// BrowswerRouter
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=like_count`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
